0684dac58573de0cdabfa781b3e86426b07c1b3c
Wesley Pyburn <10319195+TCNOco@users.noreply.github.com>
2024-02-07 13:43:29 +0200

    Update ffmpegwin.rb

--- Formula/f/ffmpegwin.rb
+++ Formula/f/ffmpegwin.rb
@@ -1,4 +1,4 @@
-class Ffmpeg < Formula
+class FfmpegWin < Formula
   desc "Play, record, convert, and stream audio and video"
   homepage "https://ffmpeg.org/"
   url "https://ffmpeg.org/releases/ffmpeg-6.1.1.tar.xz"
